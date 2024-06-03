6f28d54cee3de85bf58227bc469b36103bc0002c
Lucas Ferri <Luki.ferri@outlook.com>
2024-06-03 18:54:08 -0300

    declaraco de tipos + tentiva de criacao da matriz de strcuts

--- Hotel.cpp
+++ Hotel.cpp
@@ -1,20 +1,35 @@
-#include <stdio.h>
+#include <iostream>
 
 typedef struct endereco{
 	char end[40];
 	char munic[20];
 	char UF[3];
 	char cep[10];
-}typeEnd;
+}tEnd;
+
+typedef struct hospede{
+	char cpf[14];
+	char nome[40];
+	char telefone[15];
+	int idade;
+	tEnd endereco;
+	//float vhospedagem;
+}tHospede;
+
+typedef struct apto{
+	char status;
+	tHospede hospede;
+}tApto;
 
 
 main(){
-	char tab[20][14];
+	tApto Hotel[20][14];
 	int i, j;
 	
 	for(i = 20; i > 0; i = i - 1){
-		for(j=1; j < 15; j++)
-			tab[i][j] = '.';
+		for(j=1; j < 15; j++){
+			Hotel[i][j].status = 'n';
+		}
 	}
 		
 	do{
@@ -28,7 +43,7 @@ main(){
 		for(i = 20; i > 0; i = i - 1){
 			printf("%2d\t",i);
 			for(j=1; j < 15; j++)
-				printf(" %c ", tab[i][j]);
+				printf(" %c ", Hotel[i][j].status);
 			printf("\n");
 		}
 		
@@ -40,6 +55,6 @@ main(){
 			printf("coordenadas invalidas\n");
 			continue;
 		}
-		tab[i][j] = 'R';
+		Hotel[i][j].status = 'R';
 	}while(i != -1 || j != -1);
 }
