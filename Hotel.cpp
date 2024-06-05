29adeed5fbee1ae6af7917fabbc7a2fc298d7fcd
Lucas Ferri <Luki.ferri@outlook.com>
2024-06-04 21:27:55 -0300

    traca de matriz char por struct + menu

--- Hotel.cpp
+++ Hotel.cpp
@@ -1,60 +1,85 @@
-#include <iostream>
+#include <stdio.h>
 
 typedef struct endereco{
-	char end[40];
+	char ender[40];
 	char munic[20];
 	char UF[3];
 	char cep[10];
-}tEnd;
+}tipoEnder;
 
 typedef struct hospede{
-	char cpf[14];
-	char nome[40];
-	char telefone[15];
-	int idade;
-	tEnd endereco;
-	//float vhospedagem;
-}tHospede;
+    char cpf[14];
+    char nome[40];
+    char telefone[15];
+    int idade;
+    tipoEnder endereco;
+    float valorHospedagem;
+} tipoHospede;
 
-typedef struct apto{
-	char status;
-	tHospede hospede;
-}tApto;
+typedef struct apartamento {
+    char status;
+    tipoHospede hospede;
+} tipoApartamento;
 
+tipoApartamento hotel[20][14];
 
-main(){
-	tApto Hotel[20][14];
-	int i, j;
-	
-	for(i = 20; i > 0; i = i - 1){
-		for(j=1; j < 15; j++){
-			Hotel[i][j].status = 'n';
-		}
-	}
-		
-	do{
-		printf("\t");
-		
-		for(j=1; j < 15; j++)
-			printf("%2d ", j);
-			
-		printf("\n\n");
-		
-		for(i = 20; i > 0; i = i - 1){
-			printf("%2d\t",i);
-			for(j=1; j < 15; j++)
-				printf(" %c ", Hotel[i][j].status);
-			printf("\n");
-		}
-		
-		printf("\nDigite as coordenadas que deseja alterar(linha, coluna) (-1 para sair)\n");
-		scanf("%d %d",&i,&j);
-		if(i == -1 || j == -1)
-			break;
-		if(i < 0 || i > 20 || j < 0 || j > 14){
-			printf("coordenadas invalidas\n");
-			continue;
-		}
-		Hotel[i][j].status = 'R';
-	}while(i != -1 || j != -1);
+void matrizHotel() {
+    for (int i = 20; i > 0; i--) {
+        for (int j = 1; j < 15; j++) {
+            hotel[i][j].status = '.';
+        }
+    }
+}
+
+void exibirMapaHotel() {
+    printf("Mapa dos apartamentos:\n");
+    printf("\t");
+    
+    for(int j = 1; j < 15; j++)
+    	printf("%2d ", j);
+    printf("\n\n");
+    
+    for (int i = 20; i > 0; i--) { 
+        printf("%2d:\t", i);
+        for (int j = 1; j < 15; j++) {
+            printf(" %c ", hotel[i][j].status);
+        }
+        printf("\n");
+    }
+}
+
+void reservarApartamento(int i, int j) {
+    if (hotel[i][j].status == '.') {
+        hotel[i][j].status = 'R';
+        printf("Apartamento %d-%d reservado com sucesso.\n", i + 1, j + 1);
+    } else {
+        printf("Apartamento %d-%d não está disponível para reserva.\n", i + 1, j + 1);
+    }
+}
+
+int main() {
+    matrizHotel();
+    int opcao, andar, apto;
+
+    do {
+        printf("\nMenu:\n1. Visualizar mapa do Hotel\n2. Reservar apartamento\n0. Sair\nEscolha uma opcao: \n");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                exibirMapaHotel();
+                break;
+            case 2:
+                printf("Informe o andar e o número do apartamento para reservar (1-20, 1-14): ");
+                scanf("%d %d", &andar, &apto);
+                reservarApartamento(andar, apto);
+                break;
+            case 0:
+            printf("Saindo...\n");
+            break;
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while (opcao != 0);
+
 }
